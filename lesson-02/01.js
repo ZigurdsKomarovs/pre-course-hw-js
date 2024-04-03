a166df0eaf9707516000589160880271b68f9389
ziglins <komarovs.zigurds@gmail.com>
2024-04-03 12:59:25 +0300

    arrow

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,6 +1,3 @@
 // code
-// function doArrow(value) {
-//     console.log(value);
-// }
-let doArrow = (value) => console.log(value);
+let doArrow = value => console.log(value);
 doArrow('Передай 5');
